12fd34b961583fae0d51f2524026bb3d37a56b6a
ivan novytskyi <vanek.rainbow86@gmail.com>
2025-05-18 20:27:53 +0200

    hw11 second try

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,6 +3,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const loadingText = document.querySelector('.loading-text');
 
 const lightbox = new SimpleLightbox('.gallery a');
 
@@ -33,9 +34,11 @@ export function clearGallery() {
 }
 
 export function showLoader() {
-  loader.classList.remove('hidden');
+  loader.classList.remove('is-hidden');
+  loadingText.classList.remove('is-hidden');
 }
 
 export function hideLoader() {
-  loader.classList.add('hidden');
+  loader.classList.add('is-hidden');
+  loadingText.classList.add('is-hidden');
 }
